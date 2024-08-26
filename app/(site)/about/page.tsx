936c3d8ff681468a3002a3e2c91e1facb02fb056
Avinash Pandey <avinash.pandey8900@gmail.com>
2024-08-26 18:10:17 +0530

    Add about page component

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -20,7 +20,7 @@ export default async function About() {
               <div className="order-2 lg:order-none">
                 <h1 className="lg:text-5xl text-4xl lg:leading-tight basis-1/2 font-bold mb-8">
                   I&apos;m {data.fullName}. I live in {data.location}, where I
-                  design the future.
+                  build websites and analyze data.
                 </h1>
 
                 <div className="flex flex-col gap-y-3 text-zinc-400 leading-relaxed">
